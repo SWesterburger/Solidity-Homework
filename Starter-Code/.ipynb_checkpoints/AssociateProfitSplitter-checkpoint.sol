9ec42058e7353c6e6728ae47b845f99642e6b8dd
SWesterburger <sjwesterburger@gmail.com>
2020-08-12 14:10:28 -0400

    debug

--- Starter-Code/.ipynb_checkpoints/AssociateProfitSplitter-checkpoint.sol
+++ Starter-Code/.ipynb_checkpoints/AssociateProfitSplitter-checkpoint.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.5.0;
 // lvl 1: equal split
 contract AssociateProfitSplitter {
     // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
-    address payable employee_one = 0x187Fc74033A5d3d92D0A3E0D4b9c68e391ccCcf2;
-    address payable employee_two = 0x9F36E7B4898Ecd481F37FEA4001888a9B0d4732D;
-    address payable employee_three = 0x8c933eE583E476D1be8D6557946C0dcC036D81ED;
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
 
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
